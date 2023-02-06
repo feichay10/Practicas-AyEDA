0cc98c97a55f0befddd7182653e8ee5f488d35bf
Cheuk Kelly Ng Pante <alu0101364544@ull.edu.es>
2023-02-06 15:02:26 +0000

    Comienzo p1

---
+++ Practica_1 _Representación_números_grandes_en_notación_posicional/main.cc
@@ -0,0 +1,23 @@
+/**
+ *
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Asignatura: Algoritmos y Estructura de Datos Avanzada
+ * Curso: 2º
+ * Práctica 1: Representación de números grandes en notación posicional
+ * @file main.cc
+ * @author Cheuk Kelly Ng Pante (alu0101364544@ull.edu.es)
+ * @brief 
+ * @version 0.1
+ * @date 2023-02-17
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+
+}
