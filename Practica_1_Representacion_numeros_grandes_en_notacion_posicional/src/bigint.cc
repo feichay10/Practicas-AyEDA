efd6871e8f7da01880d68c14e9bd9f0173b89019
Cheuk Kelly Ng Pante <alu0101364544@ull.edu.es>
2023-02-12 18:11:30 +0000

    Cambios en los constructores

--- Practica_1_Representacion_numeros_grandes_en_notacion_posicional/src/bigint.cc
+++ Practica_1_Representacion_numeros_grandes_en_notacion_posicional/src/bigint.cc
@@ -8,7 +8,7 @@
  * Práctica 1: Representación de números grandes en notación posicional
  * @file bigint.cc
  * @author Cheuk Kelly Ng Pante (alu0101364544@ull.edu.es)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2023-02-17
  *
@@ -19,46 +19,59 @@
 #include "../include/bigint.h"
 
 // Constructores
-// template<size_t Base>
-// BigInt<Base>::BigInt(long n) {
-//   if (n < 0) {
-//     sign_ = -1;
-//   }
-//   while (n != 0) {
-//     digits_.push_back(n % Base);
-//     n /= Base;
-//   }
-// }
+template <size_t Base>
+BigInt<Base>::BigInt(long n) {
+  if (n == 0) {
+    sign_ = 0;
+    digits_.push_back(0);
+  }
+  if (n < 0) {
+    sign_ = -1;
+    n = -n;
+  } else {
+    sign_ = 1;
+  }
+  while (n > 0) {
+    digits_.push_back(n % Base);
+    n /= Base;
+  }
+}
 
-// template<size_t Base>
-// BigInt<Base>::BigInt(std::string& s) {
-//   if (s[0] == '-') {
-//     sign_ = -1;
-//   }
-//   for (int i = s.size() - 1; i >= 0; i--) {
-//     if (s[i] != '-') {
-//       digits_.push_back(s[i] - '0');
-//     }
-//   }
-// }
+template <size_t Base>
+BigInt<Base>::BigInt(std::string& s) {
+  if (s.empty()) {
+    sign_ = 0;
+    digits_.push_back(0);
+  }
 
-// template<size_t Base>
-// BigInt<Base>::BigInt(const char* s) {
-//   if (s[0] == '-') {
-//     sign_ = -1;
-//   }
-//   for (int i = strlen(s) - 1; i >= 0; i--) {
-//     if (s[i] != '-') {
-//       digits_.push_back(s[i] - '0');
-//     }
-//   }
-// }
+  std::string big_number = s;
+  digits_.clear();
+  if (big_number[0] == '-') {
+    sign_ = -1;
+    big_number.erase(0, 1);
+  } else {
+    sign_ = 1;
+  }
+  for (int i = big_number.size() - 1; i >= 0; i--) {
+    if (isdigit(big_number[i])) {
+      digits_.push_back(big_number[i] - '0');
+    } else if (big_number[i] >= 'A' && big_number[i] <= 'F') {
+      digits_.push_back(big_number[i] - 'A' + 10);
+    }
+  }
+}
 
-// template<size_t Base>
-// BigInt<Base>::BigInt(const BigInt<Base>& n) {
-//   sign_ = n.sign_;
-//   digits_ = n.digits_;
-// }
+template <size_t Base>
+BigInt<Base>::BigInt(const char* s) {
+  std::string str(s);
+  *this = BigInt(str);
+}
+
+template <size_t Base>
+BigInt<Base>::BigInt(const BigInt<Base>& n) {
+  sign_ = n.sign_;
+  digits_ = n.digits_;
+}
 
 // // Asignacion
 // template<size_t Base>
