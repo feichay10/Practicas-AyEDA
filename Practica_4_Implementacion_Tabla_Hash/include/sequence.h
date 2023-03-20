854b85eb4bba6d78dae1e06de441624f2b07a170
feichay10 <alu0101364544@ull.edu.es>
2023-03-20 19:17:38 +0000

    Definicion de la clase Sequence

--- Practica_4_Implementacion_Tabla_Hash/include/sequence.h
+++ Practica_4_Implementacion_Tabla_Hash/include/sequence.h
@@ -18,8 +18,12 @@
 #ifndef SEQUENCE_H
 #define SEQUENCE_H
 
+template <class Key>
 class Sequence {
   public:
+    virtual bool search(const Key& k) const = 0;
+    virtual bool insert(const Key& k) = 0;
+    virtual bool ifFull() const = 0
 };
 
 #endif // SEQUENCE_H
