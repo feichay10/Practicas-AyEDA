9ae879db1b61b7efea2cc508785eec7ce069a8c7
feichay10 <alu0101364544@ull.edu.es>
2023-04-19 09:19:00 +0100

    Init p6

---
+++ Practica_6_Implementacion_TDA_Arbol/src/main.cc
@@ -0,0 +1,30 @@
+/**
+ *
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Asignatura: Algoritmos y Estructura de Datos Avanzada
+ * Curso: 2º
+ * Práctica 6: Implementación del TDA Árbol
+ * @file main.cc
+ * @author Cheuk Kelly Ng Pante (alu0101364544@ull.edu.es)
+ * @brief
+ * @version 0.2
+ * @date 2023-05-12
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#include <iostream>
+
+const std::string kRedBold = "\033[31m\033[1m";
+const std::string kGreenBold = "\033[32m\033[1m";
+const std::string kCyanBold = "\033[36m\033[1m";
+const std::string kBold = "\033[1m";
+const std::string kReset = "\033[0m"; 
+
+int main() {
+
+}
+
