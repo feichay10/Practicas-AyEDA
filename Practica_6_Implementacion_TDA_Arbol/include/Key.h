e9b14494ef20f76c7bbb1ac083a971419ce119b4
feichay10 <alu0101364544@ull.edu.es>
2023-04-22 06:41:59 +0100

    Adding default constructor

---
+++ Practica_6_Implementacion_TDA_Arbol/include/Key.h
@@ -0,0 +1,28 @@
+/**
+ *
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Asignatura: Algoritmos y Estructura de Datos Avanzada
+ * Curso: 2º
+ * Práctica 6: Implementación del TDA Árbol
+ * @file main.cc
+ * @author Cheuk Kelly Ng Pante (alu0101364544@ull.edu.es)
+ * @brief
+ * @version 0.2
+ * @date 2023-05-12
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+template <typename T>
+class Key {
+ public:
+  Key(T key) : key_(key) {}
+  ~Key() = default;
+  T getKey() const { return key_; }
+
+ private:
+  T key_;
+};
